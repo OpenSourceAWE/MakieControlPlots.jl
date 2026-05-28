52860ec1212c051d346e757b57f54b51c262ca03
Bart <bart@vandelint.net>
2026-05-28 16:08:25 +0200

    Fix bode plot

--- src/controls.jl
+++ src/controls.jl
@@ -131,7 +131,7 @@ function _add_controls!(fig::Figure, axes_list::AbstractVector,
         for ax in axes_list
             Makie.is_mouseinside(ax.scene) || continue
             mpos = Makie.mouseposition(ax.scene)
-            inv_tf = Makie.inverse_transform(ax.scene.transform_func[])
+            inv_tf = Makie.inverse_transform(Makie.transform_func(ax.scene))
             x, y = Makie.apply_transform(inv_tf, mpos)
             cursor[] = @sprintf("x=%.4g  y=%.4g", x, y)
             crosshair_pt[] = Point2f(x, y)
